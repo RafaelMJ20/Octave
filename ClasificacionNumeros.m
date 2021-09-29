bb095d3fbd132b968cd49b28dbf4f3dd2044f092
RAFAEL MIRANDA JIMENEZ <91084460+RafaelMJ20@users.noreply.github.com>
2021-09-29 13:45:44 -0500

    CALCULO DIFERENCIAL
    Axiomas numeros reales

--- ClasificacionNumeros.m
+++ ClasificacionNumeros.m
@@ -1,11 +1,11 @@
 % Octave Script
 % Title			:Clasificación de los números
-% Description		:Script para recordar conceptos de números
+% Description		:Script para comprobar propiedades(axiomas) de los números reales
 % Author		:Rafael Miranda Jimenez
 % Date			:20210928
 % Version		:1
-% Usage			:octave>cd /path/
-%			:octave> ClasificaciónNumeros
+% Usage			:Octave>cd /path/
+%			:Octave> ClasificacionNumeros
 %			:Requiere aplicación octave, usar su línea de comandos
 %			:https://octaveintro.readthedocs.io/en/latest/index.html
 
@@ -18,90 +18,147 @@ c_numeros_Reales = 'R {I,Q,Z,N}';
 
 % Propiedades de los números, sean a,b,c,d,e ER
 
+disp('Comprobacion de axiomas algebraicos:');
+
 % Propiedades de E(Cerradura)
-p_cerradura = 'a + b ER';
-p_cerradura2 = 'ab ER';
-p_cerradura3 = '7 + 9 EN';
-p_cerradura4 = 'E= pertenecia';
+disp ('Propiedad de cerradura:');
+p_cerradura_adiccion = 'a + b ER';
+p_cerradura_producto = 'ab ER';
+disp ('Ejemplo de adiccion:');
+disp ('3 + 5 = 8 ER');
+disp ('Ejemplo de producto:');
+disp ('(-3) (2) = -6 ER');
+p_cerradura = 'E = pertenecia';
+
 a=3;
 b=5;
 a+b
+
+a=-3;
+b=2;
 a*b
 
-
-% Propiedad asociativa
-p_asociativa = 'a + (b + c ) = (a + b) + c';
-p_asociativa2 = 'a ( b c ) = ( a b ) c';
-p_asociativa3 = '3 + ( 8 - 10 ) = ( 8 + 3 ) - 10';
-a=3;
-b=8;
-c=10;
-a+ b + c
-a+b + c
-a* b*c
-a* b*c
-
 % Propiedad conmutativa
-p_conmutativa = 'a + b = b + a';
-p_conmutativa2 = ' a b = b a';
-a=10;
-b=15;
+disp ('Propiedad conmutativa:');
+p_conmutativa_adiccion = 'a + b = b + a';
+p_conmutativa_producto = ' a * b = b * a';
+disp ('Ejemplo de adiccion:');
+disp ('3 + 5 = 5 + 3');
+disp ('Ejemplo de producto:');
+disp ('(3) (2) = (2) (3)');
+
+a=3;
+b=5;
 a+b
 b+a
+
+a=3;
+b=2;
 a*b
 b*a
 
-% Propiedad distributiva
-p_distributiva = 'a ( b + c ) = a b + a c';
-a=10
-b=20
-a * (b+c)
-a*b + a*c
-% Neutro aditivo
-p_neutroA = 'a + 0 = a';
-p_neutroA2 = 'Ojo: a + 0 = 0 + a ---> es conmutativa';
-a=50
-a+0
-a+0
-0+a
+% Propiedad asociativa
+disp ('Propiedad asociativa:');
+p_asociativa_adiccion = 'a + (b + c ) = (a + b) + c';
+p_asociativa_producto = 'a ( b c ) = ( a b ) c';
+disp ('Ejemplo de adiccion:');
+disp ('1 + ( 3 + 4 ) = ( 1 + 3 ) + 4');
+disp ('Ejemplo de producto:');
+disp ('2 * ( 3 * 4 ) = ( 2 * 3 ) * 4 ')
+a=1;
+b=3;
+c=4;
+a+(b + c)
+(a+b)+ c
 
-% Neutro multiplicativo 
-p_neutroM = 'a ( 1 ) = a';
-a=10
+a=2;
+b=3;
+c=4;
+
+a*(b*c)
+(a*b)*c
+
+
+% Elemento Neutro
+disp ('Elementro neutro:');
+p_elemento_neutro_adiccion = 'a + 0 = a';
+p_elemento_neutro_producto = 'a * 1 = a';
+disp ('Ejemplo de adiccion:');
+disp ('5 + 0 = 5');
+disp ('Ejemplo de producto:');
+disp ('7 * 1 = 7');
+
+a=5;
+a+0
+
+a=7;
 a*1
 
-% Inverso aditivo
-p_inversoA = 'a + -a = 0';
-a=25
-a+ -a
+% Propiedad inversa
+disp ('Propiedad inversa:');
+p_propiedad_inversa_adiccion = 'a + ( -a ) = 0';
+p_propiedad_inversa_producto = 'a *  1/a = 1';
+disp ('Ejemplo de adiccion:');
+disp ('2 + (-2) = 0');
+disp ('Ejemplo de producto');
+disp ('5 * 1/5 = 1');
 
-% Inverso multiplicativo o recíproco
-p_inversoM = 'a ( 1/a ) = 1';
-a=15
-1/a 
+a=2;
+a+(-a)
+
+a=5;
+a*1/a
+
+% Propiedad distributiva
+disp ('Propiedad distributiva:');
+p_distributiva = 'a ( b + c ) = a * b + a * c';
+disp ('Ejemplo:');
+disp ('2(7+3) = 2*7 + 2*3');
+
+a=2;
+b=7;
+c=3;
+a * (b+c)
+a*b + a*c
+
+disp ('Axiomas de orden:');
+% Tricotomia (raíz del algebra) siempre se pueden comparar
+disp ('Tricotomia:');
+p_tricotomia = 'a < b';
+p_tricotomia2 = 'a > b';
+p_tricotomia3  = 'a = b';
+disp ('Ejemplo:');
+
+disp ('1<2');
+a=1; 
+b=2;
+a<b
+disp ('1 es menor que 2');
+
+disp ('2>1');
+a=2;
+b=1;
+a>b
+disp ('2 es mayor que 1');
+
+disp ('3=3');
+a=3;
+b=3;
+a=b
+disp ('3 es igual a 3');
 
 % Propiedad transitiva (| entonces)
+disp ('Propiedad transitiva:');
 p_transitiva = 'si a > b y b > c | a > c';
 p_transitiva2 = 'si a = b y b = c | a = c';
-a=10
-b=5
-c=2
-a>b
-b>c
-a>c
-a=b
-b=c
-a=c
+disp ('Ejemplo:');
+disp ('a>b y b>c | a>c = 5>2  2>1 | 5>1');
+disp ('Practicamente dice que si el valor de a > b y b > c entonces a > c');
+disp ('a=b b=c | a=c 5=5 5=5 5=5');
+disp ('Y en el otro caso si a = b y b = c entonces a = c');
 
-%Tricotomia (raíz del algebra) siempre se pueden comparar
-p_tricotomia = 'a > b';
-p_tricotomia2 = 'a = b';
-p_tricotomia3 = 'a < b';
-a=20
-b=15
-a > b
-a = b
-a < b
+
+
 
 % signos de agrupación
 s_agrupacion = '{ [ ( ) ] | }';
