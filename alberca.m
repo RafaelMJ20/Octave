321a5f4c3c8cf3e97fd85576a77fed26d96ff986
RAFAEL MIRANDA JIMENEZ <91084460+RafaelMJ20@users.noreply.github.com>
2021-10-19 18:38:12 -0500

    Update alberca.m

--- alberca.m
+++ alberca.m
@@ -10,14 +10,14 @@
 %limpiar variables
   clear 
 % Minimo material para la alberca
-% Rango de 0..7 i= 0.1
-  r= 6.3:0.1:6.6;
+% Rango de 0..20 i= 0.1
+  r= 0:0.1:20;
 % Valor de la función 
   ar= @(r) ((2*r)+(80/r));   
 % Funcion a plotear
   z= (2*r) + (80./r);
 % Funcion para valor minimo
-  rest= fminbnd(ar,6.3,6.6);
+  rest= fminbnd(ar,0,20);
 % Dibujar h,z
   plot(r,z)
 % Titulo
@@ -25,7 +25,7 @@
 % Etiqueta para x
   xlabel(['Min z= ' num2str(rest) ] );
 % Etiqueta para y
-  ylabel(['Min z= ' num2str( ar(rest))] );
+  ylabel(['Min z= ' num2str( ar(rest))] ); 
   
  
   
